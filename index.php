924e6352c5c5d5b52979fcafa6d60a0b6bb4e482
roryjamesallen <rory.james.allen1@gmail.com>
2025-05-15 15:05:50 +0100

    read write tiles from txt

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+function saveBoard($board) {
+    file_put_contents('game.txt', json_encode($board));
+}
+
+function readBoard() {
+    return json_decode(file_get_contents('game.txt'), true);
+}
+
+$tile_types = [
+    "",
+    "double-letter",
+    "triple-letter",
+    "double-word",
+    "triple-word"
+];
+
+$initial_board = readBoard();
+
+?>
+
+<html>
+    <head>
+    <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+     <div class="scrabble-board">
+     <?php
+     foreach ($initial_board as $tile_index => $tile) {
+         $tile_letter = $tile[1];
+         $tile_class = $tile_types[$tile[0]];
+         echo "<div id='tile-".$tile_index."' class='scrabble-tile ".$tile_class."'>".$tile_letter."</div>";
+     }
+     ?>
+     </div>
+    </body>
+</html>
