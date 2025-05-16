f971624d17e89665887ec9b0c59e8fda0c08508a
roryjamesallen <rory.james.allen1@gmail.com>
2025-05-16 15:27:35 +0100

    nearly posting

--- index.php
+++ index.php
@@ -24,12 +24,17 @@ function renderBoard($board) {
         } else {
             $tile_class = $tile_types[$tile[0]];
         }
-        echo "<input name='".$tile_index."' value='".$tile_letter."' class='scrabble-tile ".$tile_class."' onclick='attemptTileMove(this)'/>";
+        echo "<input name='board-tile-".$tile_index."' value='".$tile_letter."' class='scrabble-tile ".$tile_class."' onclick='attemptTileMove(this)'/>";
     }
 }
 
 /* If php posted to itself then update file values before anything else */
 if (!empty($_POST)) {
+
+    
+    $topleft = $_POST['board-tile-0'];
+    echo '<h1>'.$topleft.'</h1>';
+    
 	$new_board_array = $_POST['new_board_array'];
 	$new_tilebag = $_POST['new_tilebag'];
 	$new_hand = $_POST['new_hand'];
@@ -59,18 +64,16 @@ $initial_hand = readArrayFile('user_hand_1.txt');
 
          <div id="hand" class="hand">
          <?php foreach ($initial_hand as $tile_index => $tile_letter) {
-             echo "<input name='".$tile_index."' value='".$tile_letter."' class='scrabble-tile letter-tile' onclick='setHolding(this)'/>";
+             echo "<input name='hand-tile-".$tile_index."' value='".$tile_letter."' class='scrabble-tile letter-tile' onclick='setHolding(this)'/>";
          } ?>
          </div>
 	 
          <input id='holding-tile' value="" class='scrabble-tile letter-tile'></div>
 	     <div id='recall-button' class='scrabble-tile letter-tile button' onclick="recallHand()">Recall</div>
      
-         <input type="submit" value="" id="go-button" name="go-button"></input>
-         <div id='go-button' class='scrabble-tile letter-tile button'>Make Turn</div>
+         <input type="submit" value="Make Turn" name="go-button" class="scrabble-tile letter-tile button"></input>
 	</form> 
     </div>
-
 
     <script>
     function setHolding(tile) {
@@ -111,41 +114,13 @@ $initial_hand = readArrayFile('user_hand_1.txt');
 				}
 			}
 	}
-	
-	function generateBoardArray() {
-		board = document.getElementById("scrabble-board");
-		board_array = [];
-		for (const tile of board.children) {
-			tile_id = tile.id;
-			tile_index = tile_id.split('-');
-			tile_letter = tile.innerHTML;
-			tile_classes = tile.classList;
-			if (tile_classes.contains('double-letter')) {
-				tile_class = 1;
-			} else if (tile_classes.contains('triple-letter')) {
-				tile_class = 2;
-			} else if (tile_classes.contains('double-word')) {
-				tile_class = 3;
-			} else if (tile_classes.contains('triple-word')) {
-				tile_class = 4;
-			} else {
-				tile_class = 0;
-			}
-			board_array.push([tile_class, tile_letter]);
-		}
-		return board_array;
-	}
-	
-	function removeLastChar(string) {
-		return string.substring(0, string.length - 1); /* Remove last comma */
-	}
-	
+
 	function makeTurn() {
 		board_array = generateBoardArray();
 		hand = document.getElementById("hand");
 		final_hand = [];
 		for (const tile of hand.children) {
-			if (tile.innerHTML == "") { /* Tile that needs replacing */
+			if (tile.value == "") { /* Tile that needs replacing */
 				if (tilebag.length != 0) {
 					replacement_tile = tilebag[Math.floor(Math.random() * tilebag.length)];; /* Pick a random tile from the bag */
 					final_hand.push(replacement_tile); /* Put the tile in the user's hand */
@@ -157,8 +132,6 @@ $initial_hand = readArrayFile('user_hand_1.txt');
 				final_hand.push(tile.innerHTML);
 			}
 		}
-		
-	    $.post( "index.php", { new_board_array: board_array, new_tilebag: tilebag, new_hand: final_hand } );
 	}
     </script>
     </body>
