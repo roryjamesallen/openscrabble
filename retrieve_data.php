ab7533b4435907d9ac33a9673dc0d1475dc5fc77
roryjamesallen <rory.james.allen1@gmail.com>
2025-05-19 16:11:08 +0100

    hand retrieval

--- retrieve_data.php
+++ retrieve_data.php
@@ -10,8 +10,9 @@ sleep(1);
 
 $board = readArrayFile('board.txt');
 $red_hand = readArrayFile('red_hand.txt');
+$blue_hand = readArrayFile('blue_hand.txt');
 
 header('Content-Type: application/json');
-echo json_encode([$board, $red_hand]);
+echo json_encode([$board, $blue_hand, $blue_hand]);
 
 ?>
